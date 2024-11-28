47e83e36157dd862b7c5ea9d8566a39bacc5798c
Tun <tunkedsaro@gmail.com>
2024-11-28 15:46:20 +0700

    Get folder, Plus_folder, Time_folder

--- Get_folder/app.py
+++ Get_folder/app.py
@@ -25,6 +25,26 @@ async def add_numbers(num1: float, num2: float):
         result = client_socket.recv(1024).decode()   # <- Receive data 1024 byte from server then decode it
         client_socket.close()                        # close connection with Plus service
         result = float(result)                       # Convert result to float 
+
+##################################################################################################################
+
+        client_socket2 = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_STREAM
+        )
+        client_socket2.connect(
+            ("time_service", 8082)
+        )
+        message = f"{result}"
+        encode_message = message.encode()
+        client_socket2.sendall(encode_message)        # ->
+
+        # Receive the result
+        result = client_socket2.recv(1024).decode()   # <- Receive data 1024 byte from server then decode it
+        client_socket2.close()                        # close connection with Plus service
+        result = float(result)                       # Convert result to float 
+
+##################################################################################################################
         return {"result": result}                    # then return it
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))   # Raise error message
